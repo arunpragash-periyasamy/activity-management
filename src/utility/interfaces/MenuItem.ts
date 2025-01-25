d9fd6430dcfb4f0dc6da40419a5e7ea58cb9bcd6
Arunpragsh A P <aparunpragash@gmail.com>
2025-01-26 00:20:40 +0530

    Corrected the errors

--- src/utility/interfaces/MenuItem.ts
+++ src/utility/interfaces/MenuItem.ts
@@ -1,9 +1,9 @@
-import { IconType } from "react-icons";
+import React from "react";
 import { MenuItemChild } from "./MenuItemChild";
 
- export interface MenuItemInterface {
-    label: string;
-    href?: string;
-    icon: any;
-    children?: MenuItemChild[]; // Optional, as not all items will have children
-  }
+export interface MenuItemInterface {
+  label: string;
+  href?: string;
+  icon: string | React.ReactElement;
+  children?: MenuItemChild[]; // Optional, as not all items will have children
+}
